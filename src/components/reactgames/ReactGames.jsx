764a64f02c0007b26f52ed18a2a4d475d436c039
Carin Wood <carin.wood@brightstraining.com>
2023-03-24 23:19:18 +0100

    update

--- src/components/reactgames/ReactGames.jsx
+++ src/components/reactgames/ReactGames.jsx
@@ -2,6 +2,7 @@ import { Button } from '../button/Button';
 import Headline from '../headline/Headline';
 import './react.css';
 import { BsFillSuitHeartFill, BsFillSuitSpadeFill, BsSuitSpadeFill } from "react-icons/bs";
+import Playcards from './playcards.png'
 
 
 
@@ -11,24 +12,10 @@ export const ReactGames = () => {
         <Headline text={'react.js games'}/>
         <section className='bj'>
             <p className='game-title'>Black Jack</p>
-            <div className='card'>
-                <p className='letter'>K</p>
-                <BsFillSuitHeartFill className="heart"/>
-            </div>
-
-            <div className='card2'>
-                <div className='box'>
-                <p className='bokstav'>A</p>
-                <BsSuitSpadeFill className="spade"/>
-                </div>
-                <BsFillSuitSpadeFill className='big'/>
-            
-                <p className='letter2'>A</p>
-                <BsSuitSpadeFill className="spade2"/>
-                       
-            </div>
-
+            <img src={Playcards} alt="" />
+            <a href="https://carinwood-black-jack-game.netlify.app/" target="_blank">
             <Button/>
+            </a>
     
         </section>
     </div>
